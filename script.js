860b4a263590193eb9f28359eb4639f343892c4c
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-30 17:10:33 +0100

    add structur html + generetaore numeri + setTimeou

---
+++ script.js
@@ -0,0 +1,45 @@
+// generero in modo casuale 5 numeri compresi da 1 a 100 
+// li salvo in un array  
+// e stampo a schermo il risultato 
+// resta a schermo per 30 secondi 
+
+// ora parte il gioco 
+// dopo che i numeri sono spariti l'utente deve reinserire manualmente i numeri e verificare 
+// ci saranno due versioni 
+// versione 1 --> l'ordine non e importante 
+// versione 2 --> ordine importante 
+
+
+// fine del gioco e controllo dei risultati 
+
+
+
+// funzione che genera i numeri 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+  
+
+
+// funzinone che ne genera 5 e poi li inserisce in un array
+function generaEstampaInArray(){
+    const array= [];
+
+    for(let i = 0; i < 5; i++){
+        const numeroCasuale= getRandomIntInclusive(1,100);
+        array.push(numeroCasuale)
+    }
+    return array;
+
+}
+
+const numeriGenerati = generaEstampaInArray();
+document.getElementById("numeri-casuali").textContent="ecco i numeri estratti: " + numeriGenerati
+console.log("Numeri casuali generati:", numeriGenerati);
+
+setTimeout(function(){
+    document.getElementById("numeri-casuali").textContent= "";
+
+},3000);
